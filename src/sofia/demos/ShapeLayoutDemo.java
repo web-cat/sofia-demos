1a03248cf91a16352ec577b5f653d03e7b843bfa
Tony Allevato <allevato@vt.edu>
2012-12-14 08:38:49 -0500

    Modified ShapeLayoutDemo to test z-index fixes.

--- src/sofia/demos/ShapeLayoutDemo.java
+++ src/sofia/demos/ShapeLayoutDemo.java
@@ -1,34 +1,72 @@
 package sofia.demos;
 
+import java.util.Set;
+
 import sofia.app.*;
 import sofia.graphics.*;
-import static sofia.graphics.Anchor.*;
 
 public class ShapeLayoutDemo extends ShapeScreen
 {
+    private RectangleShape shape1;
+    private RectangleShape shape2;
+    private RectangleShape shape3;
+    private RectangleShape shape4;
+
     // ----------------------------------------------------------
     public void initialize()
     {
-        RectangleShape centerRect = new RectangleShape(
-            CENTER.anchoredAt(CENTER.ofView())
-                .sizedProportionalToView(0.30, 0.30));
-        centerRect.setColor(Color.red);
-        centerRect.setFilled(true);
+        shape1 = new RectangleShape(100, 100, 200, 200);
+        shape1.setColor(Color.red);
+        shape1.setFilled(true);
 
-        RectangleShape topRect = new RectangleShape(
-            BOTTOM.anchoredAt(TOP.of(centerRect).shiftBy(0, -5))
-                .sizedProportionalTo(centerRect, 0.50, 0.50));
-        topRect.setColor(Color.blue);
-        topRect.setFilled(true);
+        shape2 = new RectangleShape(50, 50, 150, 150);
+        shape2.setColor(Color.yellow);
+        shape2.setFilled(true);
 
-        RectangleShape bottomRightRect = new RectangleShape(
-            BOTTOM_RIGHT.of(centerRect).shiftBy(5, 5)
-                .sizedProportionalTo(centerRect, 0.50, 0.50));
-        bottomRightRect.setColor(Color.green);
-        bottomRightRect.setFilled(true);
+        shape3 = new RectangleShape(25, 125, 125, 225);
+        shape3.setColor(Color.blue);
+        shape3.setFilled(true);
 
-        add(centerRect);
-        add(topRect);
-        add(bottomRightRect);
+        add(shape1);
+        add(shape2);
+        add(shape3);
+
+        printShapesInOrder();
+    }
+
+
+    // ----------------------------------------------------------
+    public void onTouchDown(float x, float y)
+    {
+        RectangleShape shape = getShapeAt(x, y, RectangleShape.class);
+
+        if (shape != null)
+        {
+            shape.remove();
+        }
+        else
+        {
+            shape1.setZIndex(1000);
+
+            shape4 = new RectangleShape(50, 50, 250, 250);
+            shape4.setColor(Color.green);
+            shape4.setFilled(true);
+            shape4.setZIndex(-500);
+            add(shape4);
+        }
+
+        printShapesInOrder();
+    }
+
+
+    // ----------------------------------------------------------
+    private void printShapesInOrder()
+    {
+        System.out.println("----");
+
+        for (Shape shape : getShapes())
+        {
+            System.out.println(shape);
+        }
     }
 }
